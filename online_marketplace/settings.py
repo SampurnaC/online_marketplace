a7f90a845c52bbff2b5c4ca7205c471d27aedbf3
sam <sampurna.chapagainnn@gmail.com>
2023-05-24 23:14:14 +0100

    added signup, signin, dashboard features

--- online_marketplace/settings.py
+++ online_marketplace/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+LOGIN_URL='/login/'
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'item'
+    'item',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
